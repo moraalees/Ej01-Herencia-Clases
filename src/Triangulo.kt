35394510679aa9efe1adc09535b88c2c4848c34c
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-02-11 19:07:56 +0100

    elimino el valor lado porque es el mismo que base

--- src/Triangulo.kt
+++ src/Triangulo.kt
@@ -1,13 +1,13 @@
 package src
 
-class Triangulo(color: String, val base: Double, val altura: Double, val lado: Double): Figura(color) {
+class Triangulo(color: String, val base: Double, val altura: Double): Figura(color) {
 
     override fun area(): Double {
         return (base * altura) / 2
     }
 
     override fun perimetro(): Double {
-        return lado * 3
+        return base * 3
     }
 
 }
